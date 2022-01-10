26b01b2c68ee8b6ecaa64e58fb2820f7e055573e
mkcnelis <maikel.nelis@gmail.com>
2022-01-10 14:15:33 +0100

    fix?
    trying to fix this

--- Discolister/MainWindow.xaml.cs
+++ Discolister/MainWindow.xaml.cs
@@ -32,36 +32,20 @@ namespace Discolister
         SQLiteConnection connection = new SQLiteConnection(App.sDataBasePath);
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 3b95cd1 (version1.0.0)
+
             connection.Table<Users>().ToList().ForEach((each) =>
->>>>>>> parent of 3b95cd1 (version1.0.0)
-            {
-                if (!ValidLogin)
+
+                         if (each.eUserName == txtUsername.Text && each.ePassword == txtPassword.Text)
                 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    string Username = txtUsername.Text;
-                    string Password = txtPassword.Text;
+
                     
-                    string Statment;
-                    //DBConnections
-                    SQLiteConnection connection = new SQLiteConnection(App.sDataBasePath);
-                    {
-                        //SQLite.Open();
-                        Statment = $"SELECT EXISTS FROM Users WHERE Username='eUserName'";
-                        if((Int64) new SQLiteCommand(Statment, sSQLite).ExecuteScalar() == 0) 
-                        {
-                            
-                        }
-                    }
+             /*   }
+                else if (!Regex.IsMatch(each.eUserName, int.Users.User_ID == each.ePassword, int.Users.User_ID))
+                {*/
+                    GrantedAcces(); return;
+
                     
-=======
-=======
->>>>>>> parent of 3b95cd1 (version1.0.0)
+
 
                     
              /*   }
@@ -73,21 +57,17 @@ namespace Discolister
                 else
                 {
                     Messagebox.Text = " inlog has failed"; //displayed message if login has failed
->>>>>>> parent of 3b95cd1 (version1.0.0)
-                }
+
             }
        
                   
-<<<<<<< HEAD
+
            
         }
-=======
+
             });
           }
-<<<<<<< HEAD
->>>>>>> parent of 3b95cd1 (version1.0.0)
-=======
->>>>>>> parent of 3b95cd1 (version1.0.0)
+
         // butti to go to Registation window  new
         private void btnRegister_Click(object sender, RoutedEventArgs e)
         {
@@ -95,42 +75,37 @@ namespace Discolister
             userRegistration.ShowDialog();
         }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         //=====================================func validi login========================
-        private bool ValidLogin()
-        {
-            string message = "Enter a ";
-            if(txtUsername.Text == "")
-            {
-                message += "Username";
-            }
-            if (txtPassword.Text == "")
-            {
-                if (txtUsername.Text == "")
-                {
-                    message += "and a ";
-                }
-                message += "password";
-                }
-                if (Message != "Enter a ")
-                {
-                    LoginLabelError.Text = Message;
-                LoginLabelError.Left = (LoginPanel.Width - LoginLabelError.Width) / 2;
-                LoginLabelError.Visible = true;
-                return false;
-            }
-            }
+        // private bool ValidLogin()
+        // {
+        //     string message = "Enter a ";
+        //     if(txtUsername.Text == "")
+        //     {
+        //         message += "Username";
+        //     }
+        //     if (txtPassword.Text == "")
+        //     {
+        //         if (txtUsername.Text == "")
+        //         {
+        //             message += "and a ";
+        //         }
+        //         message += "password";
+        //         }
+        //         if (Message != "Enter a ")
+        //         {
+        //             LoginLabelError.Text = Message;
+        //         LoginLabelError.Left = (LoginPanel.Width - LoginLabelError.Width) / 2;
+        //         LoginLabelError.Visible = true;
+        //         return false;
+        //     }
+        //     }
         //=====================================func validi login========================
 
 
 
 
 
-=======
->>>>>>> parent of 3b95cd1 (version1.0.0)
-=======
->>>>>>> parent of 3b95cd1 (version1.0.0)
+
         public void GrantedAcces() 
         {
         MenuScreen menuScreen = new MenuScreen();
